c3092dbaa689f387213fa3508aea8bcb36238d59
cristhian g <virutasoft@gmail.com>
2018-08-14 23:10:51 -0500

    Agregado de la iterfaz gràfica del mòdulo de productos e inserciòn de datos dummie

--- vistas/modulos/productos.php
+++ vistas/modulos/productos.php
@@ -3,10 +3,10 @@
     <section class="content-header">
       <h1>
         Administrar Productos
-        <small>Productos</small>
+        <small>Productos y Herramientas</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
         
         <li class="active">Administrar Productos</li>
       </ol>
@@ -21,24 +21,89 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+          <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarProducto">Agregar Producto</button>
+          
         </div>
         <div class="box-body">
-          Start creating your amazing application!
+            <table class="table table-bordered table-striped  dt-responsive tablas">
+              <thead>
+                <tr>
+                  <th style="width:10px">No.</th>
+                  <th>Imágen</th>
+                  <th>Código</th>
+                  <th>Descripción</th>
+                  <th>Categoría</th>
+                  <th>Stock</th>
+                  <th>Precio de Compra</th>
+                  <th>Precio de Venta</th>
+                  <th>Agregado el...</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td><img src="vistas/img/productos/default/anonymous.png" width="40px"  alt=""></td>
+                  <td>0001</td>
+                  <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                  <td>Lorem Ipsum</td>
+                  <td>20</td>
+                  <td>5.00</td>
+                  <td>10.00</td>
+                  <td>2017-12-25 12:05:32</td>
+                  <td>
+                    <div class="btn-group">
+                    <button class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>1</td>
+                  <td><img src="vistas/img/productos/default/anonymous.png" width="40px" alt=""></td>
+                  <td>0001</td>
+                  <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                  <td>Lorem Ipsum</td>
+                  <td>20</td>
+                  <td>5.00</td>
+                  <td>10.00</td>
+                  <td>2017-12-25 12:05:32</td>
+                  <td>
+                    <div class="btn-group">
+                    <button class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>1</td>
+                  <td><img src="vistas/img/productos/default/anonymous.png" width="40px" alt=""></td>
+                  <td>0001</td>
+                  <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                  <td>Lorem Ipsum</td>
+                  <td>20</td>
+                  <td>5.00</td>
+                  <td>10.00</td>
+                  <td>2017-12-25 12:05:32</td>
+                  <td>
+                    <div class="btn-group">
+                    <button class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+
+                    </div>
+                  </td>
+                </tr>
+
+              </tbody>
+            </table>
         </div>
         <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+
       </div>
       <!-- /.box -->
 
@@ -46,3 +111,109 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+
+  <!-- MODAL AGREGAR PRODUCTO -->
+
+    <!-- Modal -->
+<div id="modalAgregarProducto" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+		<form role="form" method="post" enctype="multipart/form-data">
+			<div class="modal-header" style="background: #f39c12; color: white">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Agregar Producto</h4>
+			</div>
+
+			<div class="modal-body">
+				<div class="box-body">
+
+						<!-- ENTRADA PARA CODIGO -->
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-list-ol"></i></span>
+							<input type="text" class="form-control input-lg" name="nuevoCodigo" placeholder="Ingrese el còdigo del producto" required>
+						</div>
+					</div>
+						<!-- ENTRADA PARA CODIGO -->
+
+						<!-- ENTRADA PARA DESCRIPCIÒN -->
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-align-justify"></i></span>
+							<input type="text" class="form-control input-lg"name="nuevaDescripcion" placeholder="Ingresar descripciòn" required>
+						</div>
+					</div>
+						<!-- ENTRADA PARA DESCRIPCIÒN -->
+
+
+						<!-- ENTRADA PARA SELECCIONAR CATEGORÌA -->
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-th"></i></span>
+							<select name="nuevaCategoria"  class="form-control input-lg">
+								<option value="">Seleccionar categorìa</option>
+								<option value="taladros">Taladros</option>
+								<option value="andamios">Andamios</option>
+								<option value="equipos para construcciòn">Equipos para construcciòn</option>
+							</select>
+						</div>
+					</div>
+						<!-- ENTRADA PARA SELECCIONAR CATEGORÌA -->
+
+            <!-- ENTRADA PARA STOCK -->
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-check"></i></span>
+							<input type="number" class="form-control input-lg" name="nuevoStock" min= "0" placeholder="Ingrese el stock de producto que recibe" required>
+						</div>
+					</div>
+						<!-- ENTRADA PARA STOCK -->
+
+            <!-- ENTRADA PARA EL PRECIO DE COMPRA -->
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-dollar"></i></span>
+							<input type="number" class="form-control input-lg" name="nuevoPrecioCompra" min= "0" placeholder="Ingrese el precio de compra" required>
+						</div>
+					</div>
+						<!-- ENTRADA PARA EL PRECIO DE COMPRA -->
+
+            <!-- ENTRADA PARA EL PRECIO DE VENTA -->
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-money"></i></span>
+							<input type="number" class="form-control input-lg" name="nuevoPrecioVenta" min= "0" placeholder="Ingrese el precio de venta" required>
+						</div>
+					</div>
+						<!-- ENTRADA PARA EL PRECIO DE VENTA -->
+
+						<!-- ENTRADA PARA SUBIR FOTO -->
+					<div class="form-group text-center">
+						<div class="panel text-center" style="background: orange; color: white">SUBIR IMÀGEN DEL PRODUCTO /  HERRAMIENTA</div>
+						<input class="text-center" type="file" name="nuevaImagen" id="nuevaImagen">
+						<p class="help-block">Peso máximo de la imàgen: 2Mb</p>
+						<img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail text-center" width="100px">
+					</div>
+						<!-- ENTRADA PARA SUBIR FOTO -->
+
+
+				
+				</div>
+			</div>
+
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger pull-left" data-dismiss="modal">Cerrar</button>
+
+				<button type="submit" class="btn btn-success">Guardar Producto</button>
+
+			</div>
+		</form>
+    </div>
+
+  </div>
+</div>
+
+  <!-- MODAL AGREGAR PRODUCTO -->
