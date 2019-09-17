7a8cdd5907e0a6ccab92d1bcabd67611116c9942
Cristhian <virutasoft@gmail.com>
2019-09-16 21:47:34 -0500

    SE MAQUETA EL MÓDULO PARA ADMINISTRAR VENTAS

--- vistas/modulos/ventas.php
+++ vistas/modulos/ventas.php
@@ -6,7 +6,7 @@
         <small>Ventas</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
         
         <li class="active">Administrar Ventas</li>
       </ol>
@@ -21,24 +21,89 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+          <a href="crear-venta">
+            <button class="btn btn-primary">Agregar Venta</button>
+          </a>
         </div>
         <div class="box-body">
-          Start creating your amazing application!
+            <table class="table table-hover table-bordered table-striped dt-responsive tablas">
+              <thead>
+                <tr>
+                  <th style="width:10px">No.</th>
+                  <th>Código factura</th>
+                  <th>Cliente</th>
+                  <th>Vendedor</th>
+                  <th>Forma de pago</th>
+                  <th>Neto</th>
+                  <th>Total</th>
+                  <th>Fecha</th>
+                  <th>Acciones</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+
+                <tr>
+                  <td>1</td>
+                  <td>1000010</td>
+                  <td>JUAN VILLEGAS</td> 
+                  <td>RUPERTO MOTTA</td> 
+                  <td>TC-85747485</td> 
+                  <td>100.000.00</td> 
+                  <td>119.000.00</td> 
+                  <td>2017-12-11 12:05:32</td> 
+                  <td>
+                    <div class="btn-group">
+                    <button class="btn btn-info btn-xs"><i class="fa fa-print"></i></button>
+                    <button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>1</td>
+                  <td>1000010</td>
+                  <td>TERESA ROJAS</td> 
+                  <td>CAMILO MEDINA</td> 
+                  <td>EFECTIVO</td> 
+                  <td>250.000.00</td> 
+                  <td>319.000.00</td> 
+                  <td>2017-12-11 12:05:32</td> 
+                  <td>
+                    <div class="btn-group">
+                    <button class="btn btn-info btn-xs"><i class="fa fa-print"></i></button>
+                    <button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>1</td>
+                  <td>1000010</td>
+                  <td>JUAN VILLEGAS</td> 
+                  <td>RUPERTO MOTTA</td> 
+                  <td>TC-85747485</td> 
+                  <td>100.000.00</td> 
+                  <td>119.000.00</td> 
+                  <td>2017-12-11 12:05:32</td> 
+                  <td>
+                    <div class="btn-group">
+                    <button class="btn btn-info btn-xs"><i class="fa fa-print"></i></button>
+                    <button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+
+                    </div>
+                  </td>
+                </tr>
+
+                
+                
+              </tbody>
+            </table>
         </div>
         <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+
       </div>
       <!-- /.box -->
 
@@ -46,3 +111,5 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+
